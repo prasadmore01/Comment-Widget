7d563f1e5e07f846dcce58f9391862eb2d0cf1d2
Prasad More <prasadmoremain@gmail.com>
2024-03-03 17:02:05 +0530

    Added Delete Button as well as it's functionality into code!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let input = document.querySelector("input")
 let button = document.querySelector("button")
+let commentArr = []
 
 button.addEventListener("click",()=>{
 
@@ -8,7 +9,35 @@ button.addEventListener("click",()=>{
     div.classList.add("comments")
     let comment = document.createElement("h3")
     comment.innerHTML = input.value
+    commentArr.push(input.value)
 
+    let divIn = document.createElement("div")
+    divIn.classList.add("commentFun")
+    
+
+    let deleteC = document.createElement("h5")
+    deleteC.innerHTML = "Delete"
+    deleteC.setAttribute("id","deleteComment")
+    
     mainDiv.appendChild(div)
     div.appendChild(comment)
+    div.appendChild(divIn)
+    divIn.appendChild(deleteC)
+    
+    console.log(commentArr)
+    
+    function deleteComs(){
+
+        let commentDelete = document.querySelectorAll("h5")
+        for(let i=0;i<commentDelete.length;i++){
+            commentDelete[i].addEventListener("click",()=>{
+                div.style.display="none"
+            })
+        }
+      
+    }
+    deleteComs()
+
+    
 })
+
